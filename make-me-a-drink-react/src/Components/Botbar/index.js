ab109b57bc0fdadbebf2176dd43185f7f2b4fe41
pb-totoro <jonesec2@gmail.com>
2020-07-01 22:44:54 -0400

    Fixing mobile view

--- make-me-a-drink-react/src/Components/Botbar/index.js
+++ make-me-a-drink-react/src/Components/Botbar/index.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import "./css/style.css"
 
 export default function Botbar() {
 
-   const [visible, setVisible] = useState();
 
-   // const HoverText = styled.h1 `
-   //    color: #000000;
-   //    :hover {
-   //       color: #ffffff;
-   //       display: block;
-   //    }
-   // `
 
    const scrollTop = event => {
       event.preventDefault();
